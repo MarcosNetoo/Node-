3e8a0cae73b743e59cd3e222bfd1924785e3525e
MarcosNetoo <mvfdneto@gmail.com>
2022-11-25 10:11:44 -0300

    Versão 2 -  Placar atualizado e data.

--- teste.js
+++ teste.js
@@ -3,7 +3,10 @@ var http = require('http');
 http.createServer(function
     (req, res) {
 res.writeHead(200, {'Content-Type' : 'text/html'});
-res.end('Hello World!');
-    }).listen(2020);
+res.write('<h2>Brasil 2x0 Servia</h2> <br>');
+res.write(Date().substring(16,24)+'<br>');
+res.end('Bom dia!');
+
+}).listen(2020);
 
     
