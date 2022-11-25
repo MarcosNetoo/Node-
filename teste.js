91464c7d6b59468b39d8e93223af31c71d2fe21c
MarcosNetoo <mvfdneto@gmail.com>
2022-11-25 10:42:21 -0300

    Versão 3 - Inserção da data e hora com estilização em CSS.

--- teste.js
+++ teste.js
@@ -3,7 +3,7 @@ var dia = require("./biblioteca.js");
 http.createServer(function
     (req, res) {
 res.writeHead(200, {'Content-Type' : 'text/html'});
-res.write('<h2>Brasil 2x0 Servia</h2> <br>');
+res.write('<h2>Brasil 2x0 Suica</h2> <br>');
 res.write('<p style=color:green>'+Date().substring(16,24)+'<br>');
 res.write(dia.diadasemana()+'</p><br>')
 res.end('Bom dia!');
